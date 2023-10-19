62f334a97663fe1bc278c104fa69d7d43134a1f9
Jerson Moreno <jersonmr@gmail.com>
2023-10-19 10:15:17 -0400

    Add layout with components to shop page

--- app/Livewire/Shop/Pages/ShopPage.php
+++ app/Livewire/Shop/Pages/ShopPage.php
@@ -2,11 +2,17 @@
 
 namespace App\Livewire\Shop\Pages;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class ShopPage extends Component
 {
-    public function render()
+    public function resetFilters(): void
+    {
+        // TODO: reset shop filters
+    }
+
+    public function render(): View
     {
         return view('livewire.shop.pages.shop-page');
     }
--- resources/views/livewire/shop/pages/shop-page.blade.php
+++ resources/views/livewire/shop/pages/shop-page.blade.php
@@ -1,3 +1,56 @@
-<div>
-    Esta es la pagina de productos
+<div class="container-fluid p-5">
+    <div class="row">
+        {{-- Sidebar filters --}}
+        <div
+            style="height: 90vh; !important;"
+            class="col-sm-12 col-md-4 col-lg-4 col-xl-3 col-xxl-2 overflow-auto"
+        >
+            <livewire:shop.filters.category-filter />
+
+            <div class="my-4"></div>
+
+            <livewire:shop.filters.price-filter />
+
+            <div class="my-4"></div>
+
+            <livewire:shop.filters.color-filter />
+
+            <div class="my-4"></div>
+
+            <livewire:shop.filters.size-filter />
+
+            <div class="my-4"></div>
+
+            <livewire:shop.filters.brand-filter />
+
+            <div class="my-4"></div>
+
+            <livewire:shop.filters.rating-filter />
+        </div>
+
+        {{-- Products --}}
+        <div class="col-sm-12 col-md-8 col-lg-8 col-xl-9 col-xxl-10">
+            <div class="row">
+                <div class="col-auto">
+                    <button class="btn btn-danger" wire:click="resetFilters">
+                        <i class="fas fa-undo"></i>
+                    </button>
+                </div>
+
+                <div class="col-auto">
+                    <livewire:shop.filters.per-page-filter />
+                </div>
+
+                <div class="col">
+                    <livewire:shop.filters.search-filter />
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <livewire:shop.lists.products-list />
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
